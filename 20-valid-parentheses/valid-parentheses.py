61fa1ec78a4619a4155ecad8a4eb898c33015b55
vennasandeepreddy <50512063+vennasandeepreddy@users.noreply.github.com>
2025-03-04 22:17:27 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.77%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,21 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        dict_ocl = { ')' : '(', ']' : '[', '}' : '{', }
+        tracking_dict = {'(' : ')', '[' : ']', '{' : '}'}
         stack = []
-        for i in s:
-            #if it is a closing bracket
-            if i in dict_ocl:
-                #corresponding opening bracket found
-                if stack and dict_ocl.get(i) == stack[-1]:
-                    stack.pop(-1)
-                #opening paranthesis not found
+        for i in range(len(s)):
+
+            temp = tracking_dict.get(s[i], -1)
+            #Found in dict means; opening bracket
+            if temp != -1:
+                stack.append(temp)
+            #closing bracket
+            else:
+                if len(stack) == 0:
+                    return False
+                if stack[-1] == s[i]:
+                    stack.pop()
                 else:
                     return False
-            #if it is a opening bracket
-            else:
-                stack.append(i)
-        
-        if stack:
+            
+        if len(stack) > 0:
             return False
         else:
             return True
